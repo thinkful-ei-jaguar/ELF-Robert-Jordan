622b1d4536b966509ea958fe2b14a74f26521a24
Jordan Castillo <jordanxcallaway@gmail.com>
2020-01-06 09:17:32 -0800

    refactored Features component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import React, { Component } from 'react';
 import slugify from 'slugify';
 import Cart from './Cart';
 import './App.css';
+import Features from './Features';
 
 // This object will allow us to
 // easily convert numbers into US dollar values
-
+const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD'
+});
 
 class App extends Component {
   state = {
@@ -41,41 +45,6 @@ class App extends Component {
   };
 
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
-
-    
-
-    
-
     return (
       <div className="App">
         <header>
@@ -84,7 +53,7 @@ class App extends Component {
         <main>
           <form className="main__form">
             <h2>Customize your laptop</h2>
-            {features}
+            <Features selected={this.state.selected} features={this.props.features} updateFeature={this.updateFeature} />
           </form>
           <Cart selected={this.state.selected}/>
         </main>
