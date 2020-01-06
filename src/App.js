ba00671492cad33a92e23f28b7507b6f531aa6ab
Alex <robert.perez6@g.austincc.edu>
2020-01-06 10:35:24 -0600

    refactored Cart

--- src/App.js
+++ src/App.js
@@ -8,10 +8,7 @@ import './App.css';
 
 // This object will allow us to
 // easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
-});
+
 
 class App extends Component {
   state = {
